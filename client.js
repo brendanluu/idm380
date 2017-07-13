c6c3cbdb23ba8c15578ad027733a0b3f73008b93
brendanluu <bkl34@drexel.edu>
2017-07-12 20:47:15 -0400

    week3 in class

--- client.js
+++ client.js
@@ -2,41 +2,80 @@ window.onload = function() {
   var host = location.origin.replace(/^http/, 'ws');
   var ws = new WebSocket(host);
 
-  var id ;
+  // var id ;
+  var user = new User();
+  $('.id').html(user.id);
 
 
   ws.onopen = function() {
-    id = Math.round(Math.random() * 10000);
-    $('.id').html(id);
 
+    console.log(user);
 
-    var register = {type: 'register'};
+    var msg = {
+      'type': 'loadAll',
+      'sendToAll': false,
+      'user': user
+    }
 
-    ws.send(JSON.stringify(register));
+    ws.send(JSON.stringify(msg));
+
+    var msg = {
+      'type': 'register',
+      'sendToAll': true,
+      'user': user
+    }
+
+    ws.send(JSON.stringify(msg));
+    // id = Math.round(Math.random() * 10000);
+    //
+    //
+    //
+    // var register = {type: 'register'};
+    //
+
     }
 
     ws.onmessage = function(e){
-      console.log(e);
-
       var data = JSON.parse(e.data);
+      console.log(data);
 
-      if(data.type == "clicked") {
-        $('body').append('<p> ID #' + data.id + ' clicked! </p>');
-      } else {
-        $('body').append('<p>' + data.msg +'</p>');
-      }
+      user[data.type](data);
+
+      // if(data.type == 'register'){
+      //   user.register(data);
+      // }
+      //
+      // object.method();
+      // object['method'];
+
+      // var data = JSON.parse(e.data);
+      //
+      // if(data.type == "clicked") {
+      //   $('body').append('<p> ID #' + data.id + ' clicked! </p>');
+      // } else {
+      //   $('body').append('<p>' + data.msg +'</p>');}
 
     }
 
     $(document).mouseup(function(){
       console.log('clicked');
 
+      user.color = user.generateColor();
+
       var msg = {
-        type: 'clicked',
-        id: id
+        type:'updateColor',
+        sendToAll: 'true',
+        user: user
       };
+
       ws.send(JSON.stringify(msg));
 
-    });
+    //   var msg = {
+    //     type: 'clicked',
+    //     id: id
+    //   };
+    //   ws.send(JSON.stringify(msg));
+    //
+});
 
 }
--- server.js
+++ server.js
@@ -28,6 +28,7 @@ console.log("websocket server was created");
 
 
 var connections = [];
+var users = [];
 
 wss.on('connection', function(ws){
   connections.push(ws);
@@ -36,50 +37,77 @@ wss.on('connection', function(ws){
 
   ws.on('message', function(m){
 
-    var clientMsg = JSON.parse(m);
-    var serverMsg = {};
+		var msg = JSON.parse(m);
+		console.log(msg);
 
-    console.log(clientMsg);
+		if(msg.type == 'register'){
+			users.push(msg.user);
+			console.log(users);
+		} else if (msg.type == 'loadAll'){
+			msg.users = users;
+		}
 
-    if(clientMsg.type == 'register'){
+		if(msg.sendToAll){
+			//Send to all connections
+			users.forEach(function(user, index){
+					if(user.id == msg.id && user != msg.user) {
+						user[index] = msg.user;
+					}
+	      });
+			connections.forEach(function(connection, index){
+	        connection.send(JSON.stringify(msg));
+	        console.log("msg sent to client");
+	      });
+		}else{
+			//Send back to sender
+			ws.send(JSON.stringify(msg));
+		}
 
-
-      var time = new Date().toJSON();
-
-      serverMsg.type = clientMsg.type;
-      serverMsg.msg = time + ":someone has logged on"
-
-      connections.forEach(function(connection, index){
-        connection.send(JSON.stringify(serverMsg));
-        console.log("msg sent to client");
-      });
-    } else if (clientMsg.type == 'clicked') {
-
-      serverMsg.type = clientMsg.type;
-      serverMsg.id = clientMsg.id;
-
-      connections.forEach(function(connection, index){
-        connection.send(JSON.stringify(serverMsg));
-        console.log("msg sent to client");
-      });
-
-
-    }
+    // var clientMsg = JSON.parse(m);
+    // var msg = {};
+		//
+    // console.log(clientMsg);
+		//
+    // if(clientMsg.type == 'register'){
+		//
+		//
+    //   var time = new Date().toJSON();
+		//
+    //   serverMsg.type = clientMsg.type;
+    //   serverMsg.msg = time + ":someone has logged on"
+		//
+    //   connections.forEach(function(connection, index){
+    //     connection.send(JSON.stringify(serverMsg));
+    //     console.log("msg sent to client");
+    //   });
+    // } else if (clientMsg.type == 'clicked') {
+		//
+    //   serverMsg.type = clientMsg.type;
+    //   serverMsg.id = clientMsg.id;
+		//
+    //   connections.forEach(function(connection, index){
+    //     connection.send(JSON.stringify(serverMsg));
+    //     console.log("msg sent to client");
+    //   });
+		//
+		//
+    // }
   });
 
   ws.on('close', function(){
-    connections.splice(connections.indexOf(ws), 1);
 
-    var time = new Date().toJSON();
+    // var time = new Date().toJSON();
 
-    var serverMsg = {
+    var msg = {
       type: 'logoff',
-      msg: time + ":someone has logged off"
+      user: users[connections.indexOf(ws)]
     }
-
+//Search the connections array for the current socket is closing. Use THAT index to find the user in the users array. This index should be the same bc they are both added in the same order.
+		users.splice(connections.indexOf(ws), 1);
+		connections.splice(connections.indexOf(ws), 1);
 
     connections.forEach(function(connection, index){
-      connection.send(JSON.stringify(serverMsg));
+      connection.send(JSON.stringify(msg));
       console.log("msg sent to client");
     });
 
